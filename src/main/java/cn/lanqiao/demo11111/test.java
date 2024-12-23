2d312c7cac0b4b33ccecac4fc2beb22e5a973527
hy <2012602947@qq.com>
2024-12-23 11:24:11 +0800

    first

---
+++ src/main/java/cn/lanqiao/demo11111/test.java
@@ -0,0 +1,9 @@
+package cn.lanqiao.demo11111;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
